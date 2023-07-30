62b1b78e829c56651a2dc1f15fe75ffa25171443
Eman Awad <140901471+Emo4design@users.noreply.github.com>
2023-07-30 03:26:10 +0300

    Add files via upload

---
+++ expandingCards/script.js
@@ -0,0 +1,17 @@
+const panels=document.querySelectorAll(".panel");
+
+
+panels.forEach((panel)=>{
+    panel.addEventListener("click",
+    ()=>{removeActiveClasses();
+    panel.classList.add("active");
+});
+});
+
+
+const removeActiveClasses=()=>
+{
+    panels.forEach((pan)=>{
+        pan.classList.remove("active");
+    });
+};
